dbdd1b0b421856c6778737bf8dc5f1ecc6608f27
Iván Bazaldúa <icesarbr@gmail.com>
2020-08-27 16:14:46 -0500

    Apollo server configured correctly

--- index.js
+++ index.js
@@ -1,61 +1,22 @@
 const express = require("express");
-const graphqlHttp = require("express-graphql");
-const { makeExecutableSchema } = require("graphql-tools");
+const {ApolloServer, gql} = require('apollo-server-express');
+const cors = require('cors');
 require("dotenv").config({ path: "variables.env" });
 const DBconnection = require("./config/db");
-const resolvers = require("./graphql/resolvers");
-const typeDefs = require("./graphql/schema");
-// const { readFileSync } = require("fs");
-// const { join } = require("path");
+const { readFileSync } = require("fs");
+
 
 const app = express();
 
-app.use(express.json({ extended: true }));
+app.use(express.json({ extended: true }), cors());
 
-// prevenir errores de CORS
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, PUT, POST, PATCH, DELETE"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Content-Type, Authorization, Accept,X-Requested-With"
-  );
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(200);
-  }
-  next();
-});
 
-// leer el esquema de graphql
-// const typeDefs = readFileSync(
-//   join(__dirname, "graphql", "schema.graphql"),
-//   "utf-8"
-// );
-const executableSchema = makeExecutableSchema({ typeDefs, resolvers });
+const resolvers = require("./graphql/resolvers");
+const typeDefs = gql(readFileSync('./graphql/schema.graphql', {encoding: 'utf-8'}));
+const apolloServer = new ApolloServer({typeDefs, resolvers});
+apolloServer.applyMiddleware({app, path: '/graphql'});
 
-app.use(
-  "/graphql",
-  graphqlHttp({
-    schema: executableSchema,
-    //rootValue: resolvers,
-    graphiql: true,
-    customFormatErrorFn(err) {
-      console.log(err.originalError);
-      if (!err.originalError) {
-        console.log(err);
-        return err;
-      }
-      const data = err.originalError.data; // creado en el resolver
-      const message = err.message || "An error ocurred!"; // pull out by default by graphql
-      const code = err.originalError.code || 500;
-      console.log("data: ", data, " message: ", message, " status: ", code);
-      return { message: message, statusCode: code, data: data };
-    }
-  })
-);
+
 
 // Conectar a la base de datos
 DBconnection();
